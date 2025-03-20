8cffc2fe8d36a473387813317e224f4b8a486441
Margefson <margefson@gmail.com>
2025-03-20 14:37:06 -0400

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/machadommb/course/config/TesteConfig.java
+++ src/main/java/com/machadommb/course/config/TesteConfig.java
@@ -1,6 +1,8 @@
 package com.machadommb.course.config;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.machadommb.course.entities.Order;
 import com.machadommb.course.entities.User;
+import com.machadommb.course.repositories.OrderRepository;
 import com.machadommb.course.repositories.UserRepository;
 
 @Configuration
@@ -17,6 +21,9 @@ public class TesteConfig implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -24,11 +31,17 @@ public class TesteConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		List<User> listUsers = new ArrayList<>();
-		listUsers.add(u1);
-		listUsers.add(u2);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
 		
-		userRepository.saveAll(listUsers);
+		/*
+		 * List<User> listUsers = new ArrayList<>(); listUsers.add(u1);
+		 * listUsers.add(u2);
+		 */
+		
+		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
 	}
 
